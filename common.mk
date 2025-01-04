61824776aff31b93db2a3a52ea7995d19caefca0
Cat658011 <cat658011@gmail.com>
2025-01-04 18:17:37 +0500

    universal9611: Build libstagefright_softomx_plugin

--- common.mk
+++ common.mk
@@ -337,7 +337,8 @@ PRODUCT_COPY_FILES += \
 # VNDK
 PRODUCT_PACKAGES += \
     libnetutils.vendor:64 \
-    libsqlite.vendor:64
+    libsqlite.vendor:64 \
+    libstagefright_softomx_plugin.vendor
 
 # Sensors
 PRODUCT_PACKAGES += \
