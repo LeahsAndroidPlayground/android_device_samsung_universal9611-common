fcc45d14b5cfa30b61d3b63c2ccaf51c9c8d8d58
Leah <leah@realfx.rocks>
2025-01-07 09:17:13 +0100

    Bring in KProfiles

--- common.mk
+++ common.mk
@@ -393,3 +393,7 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_CFI_INCLUDE_PATHS += hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib
+
+# KProfiles
+PRODUCT_PACKAGES += \
+    KProfiles
